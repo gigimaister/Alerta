ce28682f403e54268058b645bd7b9098e9e7e3d5
יעקב גיגי <yaccovgigi@hotmail.com>
2021-09-12 00:19:47 +0300

    Add project files.

---
+++ Alerta/Alerta/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Alerta.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Alerta
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new SplashScreen());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
