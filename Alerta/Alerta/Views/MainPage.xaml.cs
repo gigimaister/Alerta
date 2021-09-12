27907ad49df90511ed2a8bd5f1e3fddb21f28387
יעקב גיגי <yaccovgigi@hotmail.com>
2021-09-12 22:12:41 +0300

    adding hamburger icon + nav drawer animation

---
+++ Alerta/Alerta/Views/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Alerta
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            hamburgerButton.ImageSource = (FileImageSource)ImageSource.FromFile("hambMenu48.png");
+
+            List<string> list = new List<string>();
+            list.Add("היסטוריה");
+            list.Add("בדיקה");
+            list.Add("הגדרות");
+            
+            listView.ItemsSource = list;
+        }
+
+        //Menu Clicked
+        void hamburgerButton_Clicked(object sender, EventArgs e)
+        {
+            navigationDrawer.ToggleDrawer();
+        }
+
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            
+            navigationDrawer.ToggleDrawer();
+        }
+    }
+}
